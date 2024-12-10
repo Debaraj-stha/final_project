94ac34415d0d146e9c515d2527036bb510b2775c
Debaraj-stha <dstha221@gmail.com>
2024-12-10 11:25:11 +0545

    training and testing models

--- backend/blueprints/auth.py
+++ backend/blueprints/auth.py
@@ -1,6 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint,request,jsonify,redirect
+
 
 from app import db,User
+
+
 auth = Blueprint('auth', __name__)
 def create_user(username, email, password):
     new_user = User(
@@ -11,11 +14,52 @@ def create_user(username, email, password):
     db.session.add(new_user)      
     db.session.commit()              
     return new_user
-@auth.route('/login', methods=['GET', 'POST'])
+
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return "GET request received: Render register page"
+    
+    if request.method == 'POST':
+        try:
+            data = request.json  
+            username = data.get('username')
+            email = data.get('email')
+            password = data.get('password')
+          
+            if not all([username, email, password]):
+                return jsonify({"error": "Missing fields"}), 400
+            create_user(username, email, password)
+            return jsonify({"message": "User created successfully"}), 201
+        except Exception as e:
+            return jsonify({"error": f"Error creating user: {str(e)}"}), 500
+        
+
+@auth('/login',methods=["GET","POST"])
 def login():
     try:
-        create_user("jhon","jhon@gmail.com","jhon122")
-        return "user created"
+        data=request.json
+        email = data.get('email')
+        password = data.get('password')
+
+        if not all([email, password]):
+            return jsonify({"error": "Missing fields"}), 400
+        
+        user = User.query.filter_by(email=email).first()
+        if user and User.check_password(user.password):
+            return jsonify({"message": "Logged in successfully"}), 200
+        else:
+            return jsonify({"error": "Invalid credentials"}), 401
     except Exception as e:
-        return "Error creating user: " + e.message
+        return jsonify({"error": f"Error logging in: {str(e)}"}), 500
+        
+
+
+
+
+
+
     
